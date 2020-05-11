97f80c48063d26675aadce9fd7067e3cad40d9fb
seba <dployd@hotmail.com>
2020-05-11 14:49:32 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Finanzas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
