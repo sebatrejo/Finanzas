50633c9816f934c0140d84e4a9be815d71273d1b
seba <dployd@hotmail.com>
2020-05-12 14:54:25 +0200

    Calculo van directo

---
+++ Domain/VAN.cs
@@ -0,0 +1,21 @@
+﻿namespace Finanzas.Domain
+{
+    public class VAN
+    {
+        public VAN() {}
+        public double A { get; set; }
+        public double VA { get; set; }
+        public double Qconstante { get; set; }
+        public double r { get; set; }
+        public double n { get; set; }
+        
+        public double CalculateVAN()
+        {
+            return (-1) * A + VA;
+        }
+        public double CalculateVANconstante()
+        {
+            return 0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Finanzas.Domain;
+using System;
 
 namespace Finanzas
 {
@@ -6,7 +7,62 @@ namespace Finanzas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = MainMenu();
+            }
+
+        }
+
+        private static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Elige una opción:");
+            Console.WriteLine();
+            Console.WriteLine("1) Calcular VAN");
+            Console.WriteLine("2) Calcular VAN constante");
+            Console.WriteLine("3) Salir");
+            Console.Write("\r\nSelecciona opción: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    VANcalculate();
+                    return true;
+                case "2":
+                    ConstantVANcalculate();
+                    return true;
+                case "3":
+                    return false;
+                default:
+                    return true;
+            }
+        }
+
+        private static void VANcalculate()
+        {
+            VAN _van = new VAN();
+            Console.WriteLine("VAN = -A + VA");
+            Console.WriteLine();
+            Console.WriteLine("Inserta A");
+            _van.A = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Inserta VA");
+            _van.VA = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Resultado -> " + _van.CalculateVAN());
+            Console.ReadLine();
+        }
+
+        private static void ConstantVANcalculate()
+        {
+            VAN _van = new VAN();
+            Console.WriteLine("Inserta A");
+            _van.A = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Inserta constante Qn");
+            _van.Qconstante = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Resultado -> " + _van.CalculateVAN());
+            Console.ReadLine();
         }
     }
 }
